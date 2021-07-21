208f61ac2b789acfddd65873d89a59d8d563a0aa
Pedro Esquerdo <pedroesquerdo87@gmail.com>
2021-07-21 00:24:00 -0300

    issue #2 - user registration controller added

---
+++ app/controllers/registration_controller.rb
@@ -0,0 +1,21 @@
+class RegistrationController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: 'succefully created account'
+    else
+      flash[:alert] = 'something was wrong'
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation) 
+  end
+end
